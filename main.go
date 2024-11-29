5d0e4810e24cb0651716dc9850fddd2753e4af65
Jose Leon <leonj1@gmail.com>
2024-11-29 23:37:50 +0000

    CHORE code review feedback

--- main.go
+++ main.go
@@ -1,12 +1,67 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"my-indexer/router"
 )
 
 func main() {
+	// Get port from environment variable or use default
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	r := router.NewRouter()
-	log.Fatal(http.ListenAndServe(":8080", r))
+	
+	// Configure server
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: r,
+	}
+
+	// Server run context
+	srvCtx, srvCancel := context.WithCancel(context.Background())
+	defer srvCancel()
+
+	// Channel to listen for errors coming from the listener
+	serverErrors := make(chan error, 1)
+
+	// Start server
+	go func() {
+		log.Printf("Starting server on port %s", port)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			serverErrors <- err
+		}
+	}()
+
+	// Listen for syscall signals for process to interrupt/quit
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	// Wait for signals
+	select {
+	case err := <-serverErrors:
+		log.Printf("Server error: %v", err)
+	case s := <-sig:
+		log.Printf("Server shutdown initiated by %v signal", s)
+	}
+
+	// Shutdown signal with grace period of 30 seconds
+	shutdownCtx, shutdownCancel := context.WithTimeout(srvCtx, 30*time.Second)
+	defer shutdownCancel()
+
+	// Trigger graceful shutdown
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Printf("Graceful shutdown failed: %v", err)
+	} else {
+		log.Println("Server shutdown completed")
+	}
 }
